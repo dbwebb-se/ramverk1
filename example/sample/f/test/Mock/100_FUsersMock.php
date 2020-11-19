424077a2e38230486e928dc9fb36658da9a27738
Mikael Roos <mikael.t.h.roos@gmail.com>
2020-11-19 15:22:06 +0100

    Fix php validation

--- example/sample/f/test/Mock/100_FUsersMock.php
+++ example/sample/f/test/Mock/100_FUsersMock.php
@@ -7,9 +7,6 @@ namespace Anax\Users;
  */
 class FUsersMock extends FUsers
 {
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
     protected function doCurl(string $url) : string
     {
         //var_dump($url);
@@ -37,76 +34,6 @@ class FUsersMock extends FUsers
             "lastName": "Uzzle"
         }
         EOD;
-
-        // $data = <<<EOD
-        // {
-        //     "data": [
-        //         {
-        //             "id": 1,
-        //             "firstName": "Phuong",
-        //             "lastName": "Allison"
-        //         },
-        //         {
-        //             "id": 2,
-        //             "firstName": "Marie",
-        //             "lastName": "Collinsworth"
-        //         },
-        //         {
-        //             "id": 3,
-        //             "firstName": "Simon",
-        //             "lastName": "Uzzle"
-        //         },
-        //         {
-        //             "id": 4,
-        //             "firstName": "Faustina",
-        //             "lastName": "Byfield"
-        //         },
-        //         {
-        //             "id": 5,
-        //             "firstName": "Robbie",
-        //             "lastName": "Perdue"
-        //         },
-        //         {
-        //             "id": 6,
-        //             "firstName": "Lue",
-        //             "lastName": "Lover"
-        //         },
-        //         {
-        //             "id": 7,
-        //             "firstName": "Etha",
-        //             "lastName": "Nolley"
-        //         },
-        //         {
-        //             "id": 8,
-        //             "firstName": "Jeremiah",
-        //             "lastName": "Lindauer"
-        //         },
-        //         {
-        //             "id": 9,
-        //             "firstName": "Coreen",
-        //             "lastName": "Olin"
-        //         },
-        //         {
-        //             "id": 10,
-        //             "firstName": "Josh",
-        //             "lastName": "Wayland"
-        //         },
-        //         {
-        //             "id": 11,
-        //             "firstName": "Soo",
-        //             "lastName": "Pullins"
-        //         },
-        //         {
-        //             "id": 12,
-        //             "firstName": "Shana",
-        //             "lastName": "Monty"
-        //         }
-        //     ],
-        //     "offset": 0,
-        //     "limit": 25,
-        //     "total": 12
-        // }
-        // EOD;
 
         return $data[$url];
     }
