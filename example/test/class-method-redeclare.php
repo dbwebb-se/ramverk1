29ff702c215f104f04cfe9a185a943717d106112
Mikael Roos <mikael.t.h.roos@gmail.com>
2021-01-12 17:47:44 +0100

    Add sample to demonstrate method overwrite

---
+++ example/test/class-method-redeclare.php
@@ -0,0 +1,21 @@
+<?php
+
+class A
+{
+    function m1()
+    {
+        echo __METHOD__ . "\n";
+    }
+}
+
+class B extends A
+{
+    function m1($a = null) : string
+    {
+        echo __METHOD__ . "\n";
+        return "a string";
+    }
+}
+
+$obj = new B();
+$obj->m1();
